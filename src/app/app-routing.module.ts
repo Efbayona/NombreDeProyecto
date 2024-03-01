dfecc84b500ac441fe67d4da3c94bd472c4a7027
Edwin <efbayonaa@ufpso.edu.co>
2024-02-29 20:54:47 -0500

    Maquetacion de dashboard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./modules/authentication/pages/login/login.component";
+import {DashboardComponent} from "./modules/administration/dashboard/pages/dashboard/dashboard.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: LoginComponent
+    component: DashboardComponent
   }
 ];
 

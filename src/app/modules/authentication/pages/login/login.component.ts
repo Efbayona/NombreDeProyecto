85278fa9923d06406cb848bda621cee410d5b7c7
Edwin <efbayonaa@ufpso.edu.co>
2024-03-25 10:57:41 -0500

    Ajuste url login google.

--- src/app/modules/authentication/pages/login/login.component.ts
+++ src/app/modules/authentication/pages/login/login.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AuthenticationService} from "@app/modules/authentication/services/authentication.service";
 import {MatDialog} from "@angular/material/dialog";
+import {Router} from "@angular/router";
+import {environment} from "@env/environment";
 
 @Component({
   selector: 'app-login',
@@ -10,10 +12,12 @@ import {MatDialog} from "@angular/material/dialog";
 })
 export class LoginComponent implements OnInit {
 
+  variable: string = environment.api + 'oauth2/authorization/google';
   public formLogin: FormGroup = new FormGroup({});
 
   constructor(private auth: AuthenticationService,
-              private dialog: MatDialog) {
+              private dialog: MatDialog,
+              private router: Router,) {
   }
 
 
@@ -32,7 +36,7 @@ export class LoginComponent implements OnInit {
   sendLogin(){
     console.log('ejecuta')
     if(this.formLogin.valid){
-     // this.loader.show();
+      // this.loader.show();
       const authLogin: any = {
         user_name: this.formLogin.get('user_name')?.value,
         user_password: this.formLogin.get('user_password')?.value

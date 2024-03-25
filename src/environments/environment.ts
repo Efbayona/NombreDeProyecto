5cc0d941019adae9f1cb3edbfc83264e82241fe9
Edwin <efbayonaa@ufpso.edu.co>
2024-03-25 12:12:35 -0500

    Ajuste modulo de authenticacion.

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+import {NgxLoggerLevel} from 'ngx-logger';
+
+export const environment = {
+
+  /* --- Url backend ---- */
+  api: 'http://localhost:8080/app/',
+
+  /* --- Logs ---- */
+  logLevel: NgxLoggerLevel.OFF,
+  serverLogLevel: NgxLoggerLevel.OFF,
+
+}

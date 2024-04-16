c3b8d6f4d522e57bd517aa84390e265ab2a1130d
Edwin <efbayonaa@ufpso.edu.co>
2024-04-16 16:16:22 -0500

    Input-mask

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import {MatProgressBarModule} from "@angular/material/progress-bar";
 import {ShowPasswordDirective} from "@app/shared/directives/password/show-password.directive";
 import {LoadingComponent} from "@app/shared/layouts/loading/loading.component";
+import {InputMaskDirective} from "@app/shared/directives/input-mask/input-mask.directive";
 
 @NgModule({
   declarations: [
     ShowPasswordDirective,
-    LoadingComponent
+    LoadingComponent,
+    InputMaskDirective
   ],
   imports: [
     CommonModule,
@@ -16,7 +18,8 @@ import {LoadingComponent} from "@app/shared/layouts/loading/loading.component";
   exports: [
     MatProgressBarModule,
     ShowPasswordDirective,
-    LoadingComponent
+    LoadingComponent,
+    InputMaskDirective
   ]
 })
 export class SharedModule { }

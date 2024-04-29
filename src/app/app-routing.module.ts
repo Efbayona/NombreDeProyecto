b4299848cd4f368e8c2ca75790acfb392083ff4a
Edwin <efbayonaa@ufpso.edu.co>
2024-04-28 20:40:59 -0500

    Creacion de modulo main.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     loadChildren: () => import('@app/modules/administration/administration.module').then(m => m.AdministrationModule)
   },
   {
+    path: 'home',
+    loadChildren: () => import('@app/modules/main/main-routing.module').then(m => m.MainRoutingModule)
+  },
+  {
     path: '',
     loadChildren: () => import('@app/modules/authentication/authentication.module').then(m => m.AuthenticationModule),
   }

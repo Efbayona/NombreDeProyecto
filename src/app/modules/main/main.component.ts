4dea3176a96ccd0981877e3a9fa483c6c699d98a
Edwin <efbayonaa@ufpso.edu.co>
2024-05-06 17:20:09 -0500

    Creacion del carrito.

--- src/app/modules/main/main.component.ts
+++ src/app/modules/main/main.component.ts
@@ -1,21 +1,42 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Categories} from "@app/modules/home/interfaces/home.interface";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
 
   value: boolean = false;
   categories: boolean = false;
-
   notificationList: Categories[] = [];
+  cart: boolean = false;
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+  }
 
 
   openCart() {
+    this.cart = true;
+    this.value = true;
+  }
 
+  openCategories() {
+    this.categories = true;
+    this.value = true;
+  }
+
+  navigateToHome() {
+    this.router.navigateByUrl('/app/home').then();
+  }
+
+  navigateToAuth() {
+    this.router.navigateByUrl('/').then();
   }
 
 
